69afc983a27fd53f22297eec261d1b909d565249
Kristóf Nyikes <kristof.nyikes@gmail.com>
2025-04-22 19:52:47 +0200

    feat: create initial Grid class and methods

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include "raylib.h"
+#include "grid.h"
 
 int main() {
     Color darkBlue = {44, 44, 127, 255};
     InitWindow(300, 600, "Tetris");
     SetTargetFPS(60);
+
+    Grid grid;
+    grid.Print();
 
     while (!WindowShouldClose()) {
         BeginDrawing();
