f02b08eb07daddd0cc03ed82bd4b1c9c192cb343
Kristóf Nyikes <kristof.nyikes@gmail.com>
2025-04-24 10:48:12 +0200

    feat: check for collision between blocks

--- src/game.cpp
+++ src/game.cpp
@@ -49,21 +49,21 @@ void Game::HandleInput() {
 
 void Game::MoveBlockLeft() {
     currentBlock.Move(0, -1);
-    if (IsBlockOutside()) {
+    if (IsBlockOutside() || !BlockFits()) {
         currentBlock.Move(0, 1);
     }
 }
 
 void Game::MoveBlockRight() {
     currentBlock.Move(0, 1);
-    if (IsBlockOutside()) {
+    if (IsBlockOutside() || !BlockFits()) {
         currentBlock.Move(0, -1);
     }
 }
 
 void Game::MoveBlockDown() {
     currentBlock.Move(1, 0);
-    if (IsBlockOutside()) {
+    if (IsBlockOutside() || !BlockFits()) {
         currentBlock.Move(-1, 0);
         LockBlock();
     }
@@ -94,4 +94,14 @@ void Game::LockBlock() {
     }
     currentBlock = nextBlock;
     nextBlock = GetRandomBlock();
+}
+
+bool Game::BlockFits() {
+    std::vector<Position> tiles = currentBlock.GetCellPositions();
+    for (const auto &[row, column]: tiles) {
+        if (grid.IsCellEmpty(row, column) == false) {
+            return false;
+        }
+    }
+    return true;
 }
