9b7ce8c2135fbee4a6345c469d867055be0c3cd9
Kristóf Nyikes <kristof.nyikes@gmail.com>
2025-04-24 10:41:39 +0200

    feat: blocks lock at the bottom and next block starts

--- src/game.cpp
+++ src/game.cpp
@@ -65,6 +65,7 @@ void Game::MoveBlockDown() {
     currentBlock.Move(1, 0);
     if (IsBlockOutside()) {
         currentBlock.Move(-1, 0);
+        LockBlock();
     }
 }
 
@@ -84,4 +85,13 @@ void Game::RotateBlock() {
     if (IsBlockOutside()) {
         currentBlock.UndoRotation();
     }
+}
+
+void Game::LockBlock() {
+    std::vector<Position> tiles = currentBlock.GetCellPositions();
+    for (const auto &[row, column]: tiles) {
+        grid.grid[row][column] = currentBlock.id;
+    }
+    currentBlock = nextBlock;
+    nextBlock = GetRandomBlock();
 }
--- src/game.h
+++ src/game.h
@@ -16,6 +16,7 @@ public:
 private:
     bool IsBlockOutside();
     void RotateBlock();
+    void LockBlock();
     std::vector<Block> blocks;
     Block currentBlock;
     Block nextBlock;
